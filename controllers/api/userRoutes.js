7d514d9d9cfc10be771f2715a18979898fc927ea
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-18 17:49:27 -0500

    signup debugged

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -70,15 +70,6 @@ router.post('/login', async (req, res) => {
 });
 
 // Handle user logout
-// router.post('/logout', (req, res) => {
-//   if (req.session.logged_in) {
-//     req.session.destroy(() => {
-//       res.status(204).end();
-//     });
-//   } else {
-//     res.status(404).end();
-//   }
-// });
 router.post('/logout', async (req, res) => {
   try {
     if (req.session.logged_in) {
