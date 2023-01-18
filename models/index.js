5e0e84e9c48e50599eda5c67af7944fd45a51e54
Sam Lerner <sam.a.lerner@gmail.com>
2023-01-18 11:05:38 -0500

    checking for hidden characters

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
+const User = require('./User')
 
 // Users can make multiple posts. Persist without User.
 User.hasMany(Post, {
